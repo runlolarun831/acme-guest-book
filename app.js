efad9d73a71c92f426ce3ef55b49bace13cfb8eb
Robert Wise <robcodehub@gmail.com>
2019-09-10 05:14:19 +0200

    returning guests data at /api/guests

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const fs = require('fs');
 const http = require('http');
 
+const readFile = (file) => {
+  return new Promise (( resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) {
+        reject (err);
+      } else {
+        resolve(data.toString())
+      }
+    });
+  });
+};
+
+
 http.createServer((req, res )=> {
-  res.write('hello world!');
-  res.end();
-}).listen(3000);
+  if(req.url === '/api/guests') {
+    readFile('./guests.json')
+    .then(data => {
+      res.write(data);
+      res.end();
+      })
+      .catch(ex => {
+        res.statusCode = 500;
+        res.write(ex.message);
+        res.end();
+      })
+    }}).listen(3000);
